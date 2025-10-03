ed2ee53b0e7fd0d95c024aa317fae8d0dfafd6ea
Andriy <andygrigs88@gmail.com>
2025-10-03 20:20:21 +0200

    add has_prefix method

--- task_2.py
+++ task_2.py
@@ -5,7 +5,19 @@ class Homework(Trie):
         pass
 
     def has_prefix(self, prefix) -> bool:
-       pass
+        if not isinstance(prefix, str):
+            return False
+        if prefix == "":
+            return False
+        
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False  
+            node = node.children[char]  
+        
+        return True 
+
 
 if __name__ == "__main__":
     trie = Homework()
@@ -24,3 +36,13 @@ if __name__ == "__main__":
     assert trie.has_prefix("bat") == False
     assert trie.has_prefix("ban") == True  # banana
     assert trie.has_prefix("ca") == True  # cat
+
+# if __name__ == "__main__":
+#     trie = Homework()
+#     trie.put("apple", 0)
+#     trie.put("application", 1)
+#     trie.put("banana", 2)
+    
+#     print(trie.has_prefix("app"))   # Має вивести: True
+#     print(trie.has_prefix("ban"))   # Має вивести: True
+#     print(trie.has_prefix("cat"))   # Має вивести: False
